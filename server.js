31a35cfce3ee5c69225253140c0db53a1e65882c
Razi <razi.94@gmail.com>
2018-09-11 02:08:59 +0530

    completes 1.0.0

---
+++ server.js
@@ -0,0 +1,85 @@
+const http = require('http');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+
+http.createServer((req, res) => {
+  const headers = { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "OPTIONS, POST, GET", "Access-Control-Max-Age": 2592000 }; // 30 days
+  
+  if (req.method === 'POST') {
+    let body = '';
+    let responseMessage = '';
+    req.on('data', chunk => {
+      body += chunk.toString(); // convert Buffer to string
+    });
+    req.on('end', () => {
+      const bodyData = JSON.parse(body);
+      const nonNullDataKeys = Object.keys(bodyData).filter(e => {
+        if(bodyData[e]){
+          return true;
+        }
+        return false;
+      })
+      if (nonNullDataKeys.includes('mobile') || nonNullDataKeys.includes('email')) {
+        responseMessage="ok";
+      const htmlStringArray = nonNullDataKeys.map(e => `<li><strong>${e} : </strong>${bodyData[e]}</li>`);
+      const htmlString = `<ul>${htmlStringArray.join("")}</ul>`;
+      try {
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false, // secure:true for port 465, secure:false for port 587
+          auth: {
+            user: 'aqualuxy.kannur@gmail.com',
+            pass: 'theerthamKannur'
+          }
+        });
+        // setup email data with unicode symbols
+        const _now = new Date();
+        let mailOptions = {
+          from: '"Aqualuxy" <aqualuxy.kannur@gmail.com>', // sender address
+          to: 'razi47rasheed@gmail.com',
+          subject: `Enquiry made at aqualuxy.com || ${_now.toLocaleString('en-GB')}`, // Subject line
+          text: 'following is the details of enquiry made at aqualuxy.com ?', // plain text body
+          html: `<br/>${htmlString}` // html body
+        };
+
+        // send mail with defined transport object
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        console.log("Message sent: " + info.response);
+      });
+
+    }catch(e){
+      console.log(e.message)
+    }
+    }else{
+      responseMessage('notEnoughInfo')
+    }
+    res.writeHead(200, headers)
+    res.end(responseMessage);
+  });
+  }
+
+  let {url} = req;
+  url = url === '/' ? '/index.html' : url;
+
+  if(req.method === 'GET') {
+    console.log(url)
+    // console.log(/.*\.html/gi.exec(url)[0]);
+    const fileNameString = /.*\.html/gi.test(url) ? /.*\.html/gi.exec(url)[0] : url;
+    fs.readFile(__dirname + fileNameString, (err, data) => {
+      if (err) {
+        console.log(err.message);
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('Page Not Found');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+      }
+    })
+  }
+}).listen(5500);
